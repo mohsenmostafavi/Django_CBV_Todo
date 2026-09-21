0905798ce6f7d454dc00bc3559df127d0d57599e
MOHSEN <baxdev1@gmail.com>
2026-09-22 02:09:05 +0330

    feat(accounts): add register, login, logout

--- core/settings.py
+++ core/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # Installed Apps
-    "acccounts.apps.AccountsConfig",
+    "accounts.apps.AccountsConfig",
     "todo.apps.TodoConfig",
 ]
 
